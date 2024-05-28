bf480e67edda4e8122cad3ddbdc4f6797e95714b
onkar <onkarsawant111@gmail.com>
2024-05-28 12:35:55 +0530

    updated

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+rooms = [{'id':1, 'name': 'onkar is the man'},{'id':2, 'name': 'how are you'},{'id':3, 'name': 'good morning'}]
+
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    context = {'rooms': rooms}
+    return render(request,'base/home.html',context)
 
 def room(request):
-    return render(request,"room.html")
+    return render(request,"base/room.html")

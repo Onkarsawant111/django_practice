c6ab61e0e7c177fa2ce16365cf006cbfbc11fa97
onkar <onkarsawant111@gmail.com>
2024-05-28 22:49:05 +0530

    updated

--- base/views.py
+++ base/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
-rooms = [{'id':1, 'name': 'onkar is the man'},{'id':2, 'name': 'how are you'},{'id':3, 'name': 'good morning'}]
+# rooms = [{'id':1, 'name': 'onkar is the man'},{'id':2, 'name': 'how are you'},{'id':3, 'name': 'good morning'}]
 
 # Create your views here.
-def home(request):
-    context = {'data': rooms}
-    return render(request,'base/home.html',context)
+# def home(request):
+#     context = {'data': rooms}
+#     return render(request,'base/home.html',context)
 
-def room(request,pk):
-    j = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            j = i
-    context = {'room': j}
-    return render(request,"base/room.html",context)
+# def room(request,pk):
+#     j = None
+#     for i in rooms:
+#         if i['id'] == int(pk):
+#             j = i
+#     context = {'room': j}
+#     return render(request,"base/room.html",context)
 
+
+#222. for loop django template
+# name = ["onkar",'raj','max']
+# def home(request):
+#     data = {'name':name}
+#     return render(request,'base/home.html',data)

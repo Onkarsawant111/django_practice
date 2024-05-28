5d79d0aa5ac8ea75eb1c0e7a6672837109c35ed6
onkar <onkarsawant111@gmail.com>
2024-05-28 21:37:34 +0530

    updated

--- base/views.py
+++ base/views.py
@@ -5,8 +5,14 @@ rooms = [{'id':1, 'name': 'onkar is the man'},{'id':2, 'name': 'how are you'},{'
 
 # Create your views here.
 def home(request):
-    context = {'rooms': rooms}
+    context = {'data': rooms}
     return render(request,'base/home.html',context)
 
-def room(request):
-    return render(request,"base/room.html")
+def room(request,pk):
+    j = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            j = i
+    context = {'room': j}
+    return render(request,"base/room.html",context)
+
